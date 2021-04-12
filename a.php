0330eda1da66e147ed7245eb8f6b360824dce4bb
lko12230 <77441105+lko12230@users.noreply.github.com>
2021-04-12 08:55:32 +0530

    Add files via upload

---
+++ a.php
@@ -0,0 +1,38 @@
+<?php      
+    include('config.php');  
+    if(isset($_POST['login']))
+    {
+    $username = $_POST['username'];  
+    $password = $_POST['password'];  
+      
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($conn, $username);  
+        $password = mysqli_real_escape_string($conn, $password);  
+      
+        $sql = "select * from admin where email = '$username' and password = '$password'";  
+        $result = mysqli_query($conn, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> alert('Login successful') </center></h1>";  
+            header("Location: https://www.irctc.co.in/nget/train-search");
+        }  
+        else{  
+            header("Location: http://localhost/login/");
+       }   }     
+       if(isset($_POST['forgot']))
+       {
+        header("Location: http://localhost/forgotpassword/");
+       }
+       if(isset($_POST['register']))
+       {
+        header("Location: http://localhost/register/");
+       }
+       if(isset($_POST['contact']))
+       {
+        header("Location: http://localhost/support/");
+       }
+?>  
