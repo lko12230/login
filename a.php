0735a545aef4debe21f8881362aa14bdf7a1d1f7
lko12230 <77441105+lko12230@users.noreply.github.com>
2021-04-12 21:15:16 +0530

    Update a.php

--- a.php
+++ a.php
@@ -34,7 +34,7 @@
        }
        if(isset($_POST['register']))
        {
-        header("Location: https://lko12230.github.io/register//");
+        header("Location: https://lko12230.github.io/register/");
        }
        if(isset($_POST['contact']))
        {
